a6a95bbd18b917a543529ba512a8fe4ed3489114
Karkyr <pcselection19@gmail.com>
2023-12-14 19:49:42 +0100

    Created container back-end and front-end. Issue with nginx.

---
+++ back-end/back-end/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new {Name = "Ana"},
+                new {Name = "Anatoly"}
+            };
+            return Ok(users);
+        }
+    }
+}
